74cc1623b06496ef65eaf70a162aa71d52a7955f
MiltonRodx <uztracker.herbs129@slmails.com>
2026-06-28 22:44:08 +0000

    add test

---
+++ hello.js
@@ -0,0 +1,25 @@
+// In-line comment 
+var number = 5;
+
+const pi = 3.14159
+
+/* This is a
+Multi line comment */
+
+number = 9;
+
+
+
+// Data types
+// undefined, null. boolean, string, symbol, number, object
+var amIOkay = true;
+var nodefinido = null;
+
+var numberone = 1;
+var myName = "Milton";
+var mySurname = "Rodriguez";
+
+console.log(number);
+console.log(myName);
+console.log(mySurname);
+console.log("Hola mundo!!!");
